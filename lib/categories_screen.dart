2c71b22835f6382014114d9408ef77fe0fc6f480
Akshay Bengani <akshaybengani@gmail.com>
2019-12-25 02:27:08 +0530

    Grid Layout Added

---
+++ lib/categories_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'dummy_data.dart';
+import 'category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      child: GridView(
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                title: catData.title,
+                color: catData.color,
+              ),
+            )
+            .toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/dummy_data.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/modals/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'French',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Indian',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Chinese',
+    color: Colors.redAccent,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Arabian',
+    color: Colors.purpleAccent,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Japanese',
+    color: Colors.blueAccent,
+  ),
+  Category(
+    id: 'c8',
+    title: 'American',
+    color: Colors.blueGrey,
+  ),
+
+  Category(
+    id: 'c9',
+    title: 'Antartican',
+    color: Colors.amber,
+  ),
+
+];
